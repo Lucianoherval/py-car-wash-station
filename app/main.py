fc9fbcd128407bb71f7c096c2badbd5aa9f17872
Luciano Herval <lucianoherval@gmail.com>
2026-03-18 15:08:44 -0300

    Solution

--- app/main.py
+++ app/main.py
@@ -23,22 +23,13 @@ class CarWashStation:
     def serve_cars(self, cars: list[Car]) -> float:
         total_income = 0.0
         for car in cars:
-            # Apenas lavamos carros que estão mais sujos do que o poder de lavagem da máquina
             if car.clean_mark < self.clean_power:
-                # 1. Calculamos o preço e somamos aos ganhos do dia
                 total_income += self.calculate_washing_price(car)
-                # 2. Lavamos o carro de facto (atualizamos o nível de limpeza dele)
                 self.wash_single_car(car)
-        
-        # A tarefa pede para retornar o rendimento arredondado a 1 casa decimal
+               
         return round(total_income, 1)
 
     def rate_service(self, rate: int) -> None:
-        # Para calcular a nova média, primeiro descobrimos o valor total dos votos antigos
         total_score = self.average_rating * self.count_of_ratings
-        
-        # Somamos mais uma pessoa ao total de avaliadores
         self.count_of_ratings += 1
-        
-        # Calculamos a nova média e atualizamos o atributo (arredondado a 1 casa)
         self.average_rating = round((total_score + rate) / self.count_of_ratings, 1)
